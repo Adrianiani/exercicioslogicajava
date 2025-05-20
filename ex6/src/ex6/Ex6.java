7420055382830a3dd7d6c8819ab2ff3991fdf08b
Adrianiani <monteiroadriani@gmail.com>
2025-05-20 17:19:20 -0300

    exercicio 6 feito

--- ex6/src/ex6/Ex6.java
+++ ex6/src/ex6/Ex6.java
@@ -1,11 +1,18 @@
 
-package ex6;
-
-public class Ex6 {
-
-    public static void main(String[] args) {
-        
-        
+package ex9;
+import java.util.Scanner;
+public class Ex9 {
+    public static double calculo(double valor, double porcentagem) {
+        double porcento = (valor * porcentagem) / 100;
+        return porcento;
     }
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o valor: ");
+        double valor = scanner.nextDouble();
+        System.out.print("Digite a porcentagem: ");
+        double porcentagem = scanner.nextDouble();
+        double resultado = calculo(valor, porcentagem);
+        System.out.println(porcentagem + "% de " + valor + " é: " + resultado);
+    }
 }
