bf00ca5233f03d4a05b7f24cb41b3c1ce46757ff
Adrianiani <monteiroadriani@gmail.com>
2025-05-20 17:30:59 -0300

    exercicio 6 arrumado

--- ex6/src/ex6/Ex6.java
+++ ex6/src/ex6/Ex6.java
@@ -1,18 +1,1 @@
 
-package ex9;
-import java.util.Scanner;
-public class Ex9 {
-    public static double calculo(double valor, double porcentagem) {
-        double porcento = (valor * porcentagem) / 100;
-        return porcento;
-    }
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Digite o valor: ");
-        double valor = scanner.nextDouble();
-        System.out.print("Digite a porcentagem: ");
-        double porcentagem = scanner.nextDouble();
-        double resultado = calculo(valor, porcentagem);
-        System.out.println(porcentagem + "% de " + valor + " é: " + resultado);
-    }
-}
