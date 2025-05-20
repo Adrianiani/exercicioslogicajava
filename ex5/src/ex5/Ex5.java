9343ba46ef019135119c2aa467ada7723b4caad4
Adrianiani <monteiroadriani@gmail.com>
2025-05-20 17:03:23 -0300

    exercicio 5 feito

--- ex5/src/ex5/Ex5.java
+++ ex5/src/ex5/Ex5.java
@@ -1,11 +1,25 @@
-
 package ex5;
-
 public class Ex5 {
-
-    public static void main(String[] args) {
-        
-        
+    public static int continha(int numero) {
+        if (numero < 0) {
+            return -1;
+        }
+        if (numero == 0) {
+            return 1;
+        }
+        int resultado = 1;
+        for (int i = 1; i <= numero; i++) {
+            resultado = resultado * i;
+        }
+        return resultado;
     }
-    
+    public static void main(String[] args) {
+        int numero = 5;
+        int fatorial = continha(numero);
+        if (fatorial == -1) {
+            System.out.println("O número é negativo (inválido).");
+        } else {
+            System.out.println("O fatorial de " + numero + " é: " + fatorial);
+        }
+    }
 }
